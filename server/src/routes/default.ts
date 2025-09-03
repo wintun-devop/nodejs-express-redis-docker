e65cc772a3547043fedf81966ddcf869c2cfac47
wintun-devop <wintun.hlaing@outlook.com>
2025-09-03 20:42:53 +0630

    add init

---
+++ server/src/routes/default.ts
@@ -0,0 +1,10 @@
+import { Router, Request, Response } from 'express';
+
+
+//declare router
+export const defaultRoute = Router();
+
+// crate request and response
+defaultRoute.get('/', async (req: Request, res: Response) => {
+    res.send({ "message": "API is working ok" })
+})
